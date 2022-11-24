b9ee452f6c1984e78f92d47712e02ca520cb31fd
Bill.Dagou <billdagou@gmail.com>
2022-11-24 09:48:25 +0800

    Fix NS

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,12 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['github'] = \Dagou\Github\Controller\EidController::class.'::processRequest';
-
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['be'] = [
+    'TYPO3\\CMS\\Backend\\ViewHelpers',
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['c'] = [
+    'TYPO3\\CMS\\Core\\ViewHelpers',
+];
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['github'] = [
     'Dagou\\Github\\ViewHelpers',
 ];
